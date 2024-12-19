f6b752f1061f93bbec4604ca164e7775774af32f
utsavbpatel <166483990+utsavbpatel@users.noreply.github.com>
2024-12-19 14:16:24 +0530

    Update Services.jsx

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -3,11 +3,9 @@ import './Service.css'
 
 function Services() {
     const [data, setData] = useState([]);
+    const [servicesData,setServicesData]=useState([]);
     const [isLoading, setIsLoading]=useState(true);
     const [searchQuery,setSearchQuery]=useState('');
-
-
-    // console.log("Service Name",data[0].service_name)
 
     useEffect(() => {
         const fetchservicesdata = async () => {
@@ -16,6 +14,7 @@ function Services() {
                 const response = await fetch('http://20.193.149.47:2242/salons/service/')
                 const fetcheddata = await response.json();
                 setData(fetcheddata.results);
+                setServicesData(fetcheddata.results);
                 setIsLoading(false);
             } catch (error) {
                 throw new error
@@ -27,12 +26,22 @@ function Services() {
     console.log('Fetch Data', data);
 
 
+
     const handleSearch=(search)=>{
+        if(search===""){
+            setServicesData(data)
+        }else{
         const filteredData= data.filter((value)=>(
-            value.service_name === search
+            value.service_name.toLowerCase().startsWith(search.toLowerCase())
         ))
-        setData(filteredData)      
+        setServicesData(filteredData) 
+    }  
     }
+
+    useEffect(()=>{
+        handleSearch(searchQuery)
+    },[searchQuery])
+
 
     return (
         <div>
@@ -41,9 +50,7 @@ function Services() {
             </div>
             <div className="sub-heading">
                 <h2>Services</h2>
-                <input type="text" value={searchQuery} onChange={(e)=> setSearchQuery(e.target.value) } />
-                <button onClick={()=>handleSearch(searchQuery)} ><i className="fa-solid fa-magnifying-glass"></i></button>
-                
+                <input type="text" value={searchQuery} onChange={(e)=>{ setSearchQuery(e.target.value); handleSearch(e.target.value)}} placeholder='Search by Service Name' />
             </div>
             <div className='servies-list'>
                 <table>
@@ -52,6 +59,7 @@ function Services() {
                             <th>No.</th>
                             <th>Service Name</th>
                             <th>Service Image</th>
+                            <th>Description</th>
                             <th>Service Time</th>
                             <th>Price</th>
                             <th>Discount</th>
@@ -62,15 +70,16 @@ function Services() {
                     <tbody>
                         {isLoading && 
                         <tr>
-                            <td>Loading....</td>
+                            <td colSpan="9">Loading....</td>
                         </tr>
 
                         }
-                        {!isLoading && data.map((value, index) => (
+                        {!isLoading && servicesData.map((value, index) => (
                             <tr key={index}>
                                 <td>{index+1}</td>
                                 <td>{value.service_name}</td>
                                 <td><img src={value.service_image}  /></td>
+                                <td dangerouslySetInnerHTML={{ __html: value.description }}></td>
                                 <td>{value.service_time.days} Days, {value.service_time.hours} Hours, {value.service_time.minutes} Minutes</td>
                                 <td>{value.price} Rs.</td>
                                 <td>{value.discount} Rs.</td>
